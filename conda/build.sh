7bb1c057267395d57cbe3fd01b4b5c194f6586a5
cvoegele <christoph228@protonmail.ch>
2022-03-07 10:15:08 +0000

    update post-link

--- conda/build.sh
+++ conda/build.sh
@@ -2,8 +2,7 @@ wget https://gitlab.com/ska-telescope/external/rascil/-/blob/master/LICENSE
 
 mkdir tmp
 cd tmp
-git clone https://gitlab.com/ska-telescope/external/rascil .
+git clone --branch "$PKG_VERSION" https://gitlab.com/ska-telescope/external/rascil .
 $PYTHON -m pip install . --no-deps
 cd ..
 rm -rf tmp
-
--- conda/post-link.sh
+++ conda/post-link.sh
@@ -2,9 +2,18 @@ cmd=curl
 [[ $(type -P "$cmd") ]] && echo "$cmd is in PATH"  ||
     { echo "$cmd is NOT in PATH" 1>&2; exit 1; }
 
-mkdir rascil_data
-cd rascil_data
+#cmd=wget
+#[[ $(type -P "$cmd") ]] && echo "$cmd is in PATH"  ||
+#    { echo "$cmd is NOT in PATH" 1>&2; exit 1; }
+
+
+DATA_DIRECTORY="$PREFIX/lib/rascil_data"
+echo $DATA_DIRECTORY
+mkdir $DATA_DIRECTORY
+cd $DATA_DIRECTORY
 curl https://ska-telescope.gitlab.io/external/rascil/rascil_data.tgz -o rascil_data.tgz
-tar zxf rascil_data.tgz
-cd data
-conda env config vars set RASCIL_DATA=`pwd`
+tar zxf rascil_data.tgz -C
+mv data/* .
+mv data/.* .
+rmdir data
+conda env config vars set RASCIL_DATA=$DATA_DIRECTORY
