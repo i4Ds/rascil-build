a2018b4a817f0f59990455428de24590651f50c3
Christoph Vögele <christoph228@protonmail.ch>
2022-03-14 14:08:09 +0100

    fix data directory

--- conda/post-link.sh
+++ conda/post-link.sh
@@ -13,4 +13,4 @@ mkdir $DATA_DIRECTORY
 cd $DATA_DIRECTORY
 curl https://ska-telescope.gitlab.io/external/rascil/rascil_data.tgz -o rascil_data.tgz
 tar zxf rascil_data.tgz -C
-conda env config vars set RASCIL_DATA="$DATA_DIRECTORY/data"
+conda env config vars set RASCIL_DATA="$DATA_DIRECTORY"
