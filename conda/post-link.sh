19b06e3df1147dafeef8afe8d9a9706520ef2ecf
Christoph Vögele <christoph228@protonmail.ch>
2022-03-14 15:35:12 +0100

    fix data directory

--- conda/post-link.sh
+++ conda/post-link.sh
@@ -1,16 +1,17 @@
-cmd=curl
-[[ $(type -P "$cmd") ]] && echo "$cmd is in PATH"  ||
-    { echo "$cmd is NOT in PATH" 1>&2; exit 1; }
+#cmd=curl
+#[[ $(type -P "$cmd") ]] && echo "$cmd is in PATH"  ||
+#    { echo "$cmd is NOT in PATH" 1>&2; exit 1; }
 
 #cmd=wget
 #[[ $(type -P "$cmd") ]] && echo "$cmd is in PATH"  ||
 #    { echo "$cmd is NOT in PATH" 1>&2; exit 1; }
 
 
-DATA_DIRECTORY="$PREFIX/lib/rascil_data"
+DATA_DIRECTORY="$PREFIX/rascil_data"
 echo $DATA_DIRECTORY
 mkdir $DATA_DIRECTORY
 cd $DATA_DIRECTORY
 curl https://ska-telescope.gitlab.io/external/rascil/rascil_data.tgz -o rascil_data.tgz
 tar zxf rascil_data.tgz -C
 conda env config vars set RASCIL_DATA="$DATA_DIRECTORY"
+
