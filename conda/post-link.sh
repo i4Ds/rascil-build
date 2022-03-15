a8856736a0f8014b37c50937447f47635d247ba2
Christoph Vögele <christoph228@protonmail.ch>
2022-03-15 11:50:16 +0100

    set unpack directory

--- conda/post-link.sh
+++ conda/post-link.sh
@@ -3,10 +3,8 @@ cmd=curl
     { echo "$cmd is not installed. Please install $cmd and try the installation again ex.: (conda install $cmd)" 1>&2; exit 1; }
 
 DATA_DIRECTORY="$PREFIX/rascil_data"
-echo $DATA_DIRECTORY
 mkdir $DATA_DIRECTORY
-cd $DATA_DIRECTORY
 curl https://ska-telescope.gitlab.io/external/rascil/rascil_data.tgz -o rascil_data.tgz
-tar zxf rascil_data.tgz -C
+tar zxf rascil_data.tgz -C $DATA_DIRECTORY
 conda env config vars set RASCIL_DATA="$DATA_DIRECTORY"
 
