4a0ced3a5f16cc6d21cf25c208544bd81821e21c
Christoph Vögele <christoph228@protonmail.ch>
2022-03-16 16:43:21 +0100

    update conda env variables in post-link.sh

--- conda/post-link.sh
+++ conda/post-link.sh
@@ -7,5 +7,5 @@ mkdir $DATA_DIRECTORY
 cd $DATA_DIRECTORY
 curl https://ska-telescope.gitlab.io/external/rascil/rascil_data.tgz -o rascil_data.tgz
 tar zxf rascil_data.tgz
-conda env config vars set RASCIL_DATA="$DATA_DIRECTORY"
+conda env config vars set RASCIL_DATA="$DATA_DIRECTORY/data"
 
