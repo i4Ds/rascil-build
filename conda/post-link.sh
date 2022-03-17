7923ab9bdefde7c689c733c2fc0a076579bda911
Christoph Vögele <christoph228@protonmail.ch>
2022-03-17 12:49:19 +0100

    move data directory

--- conda/post-link.sh
+++ conda/post-link.sh
@@ -2,10 +2,10 @@ cmd=curl
 [[ $(type -P "$cmd") ]] && echo "$cmd is in PATH"  ||
     { echo "$cmd is not installed. Please install $cmd and try the installation again ex.: (conda install $cmd)" 1>&2; exit 1; }
 
-DATA_DIRECTORY="$PREFIX/rascil_data"
+DATA_DIRECTORY="$PREFIX/../data/"
 mkdir $DATA_DIRECTORY
 cd $DATA_DIRECTORY
 curl https://ska-telescope.gitlab.io/external/rascil/rascil_data.tgz -o rascil_data.tgz
 tar zxf rascil_data.tgz
-conda env config vars set RASCIL_DATA="$DATA_DIRECTORY/data"
+conda env config vars set RASCIL_DATA="$DATA_DIRECTORY"
 
