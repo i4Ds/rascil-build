01cd2ef6b1abea4f079b4daa63a25d5e1a3f5d0e
lukas.gehrig <lukas.gehrig@students.fhnw.ch>
2022-10-07 16:17:14 +0200

    dev and version branches setup

--- conda/build.sh
+++ conda/build.sh
@@ -1,8 +1,10 @@
 wget https://gitlab.com/ska-telescope/external/rascil/-/blob/master/LICENSE
 
+pkgv=$(sed 's/dev//g' <<< "$PKG_VERSION")
+
 mkdir tmp
 cd tmp
-git clone --branch "$PKG_VERSION" https://gitlab.com/ska-telescope/external/rascil .
+git clone --branch "$pkgv" https://gitlab.com/ska-telescope/external/rascil .
 $PYTHON -m pip install . --no-deps
 cd ..
 rm -rf tmp
