641f50fcca55045fbf7348644b6801914b8a42bd
lukas.gehrig <lukas.gehrig@students.fhnw.ch>
2022-10-11 14:50:36 +0200

    using github environment variables for version

--- test.sh
+++ test.sh
@@ -3,12 +3,12 @@ source $CONDA_BASE/etc/profile.d/conda.sh
 
 conda create -y -n testpy38 python=3.8
 conda activate testpy38
-conda install -y -c i4ds -c conda-forge rascil="$PKG_VERSION"
+conda install -y -c i4ds -c conda-forge rascil=${{ env.VERSION }}
 python -c "import rascil"
 python -c "from rascil.apps import rascil_imager"
 
 conda create -y -n testpy39 python=3.9
 conda activate testpy39
-conda install -y -c i4ds -c conda-forge rascil="$PKG_VERSION"
+conda install -y -c i4ds -c conda-forge rascil=${{ env.VERSION }}
 python -c "import rascil"
 python -c "from rascil.apps import rascil_imager"
